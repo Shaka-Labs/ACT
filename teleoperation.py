7cc006a97fc456adf14cd38fe20c46aec76dd3c3
Seif (MAcbook M3) <vateseif@gmail.com>
2024-04-12 01:18:22 +0200

    minor

--- teleoperation.py
+++ teleoperation.py
@@ -7,8 +7,8 @@ follower = Robot(device_name=ROBOT_PORTS['follower'])
 # activate the leader gripper torque
 leader.set_trigger_torque()
 
-#while True:
-#    follower.set_goal_pos(leader.read_position())
+while True:
+    follower.set_goal_pos(leader.read_position())
 
-leader._disable_torque()
-follower._disable_torque()
+#leader._disable_torque()
+#follower._disable_torque()
