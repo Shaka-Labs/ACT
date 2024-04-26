47a50d6005450713da9566fc7dba5dd7b3fe3ffb
Antonio Arbues <avarbues@gmail.com>
2024-04-26 21:39:29 +0200

    teleop

--- teleoperation.py
+++ teleoperation.py
@@ -1,14 +1,14 @@
-from robot import Robot
-from config.config import ROBOT_PORTS
+from loki.robot import Robot
+from loki.config.config import ROBOT_PORTS
 
 # init robots
-leader = Robot(device_name=ROBOT_PORTS['leader'])
-follower = Robot(device_name=ROBOT_PORTS['follower'])
+leader = Robot(device_name=ROBOT_PORTS['leader'], servo_ids=[1, 2, 3, 4, 5, 6, 7])
+follower = Robot(device_name=ROBOT_PORTS['follower'], servo_ids=[1, 2, 3, 4, 5, 6, 7])
 # activate the leader gripper torque
 leader.set_trigger_torque()
 
 while True:
-    follower.set_goal_pos(leader.read_position())
+    follower.set_goal_pos(leader.read_position(linear=True))
 
 #leader._disable_torque()
 #follower._disable_torque()
